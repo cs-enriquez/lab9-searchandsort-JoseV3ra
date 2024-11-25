b128b9f8eff22a11f46d4bcd935d8280fdb3ca7f
JoseV3ra <143547576+JoseV3ra@users.noreply.github.com>
2024-11-25 06:01:06 +0000

    finished code

--- Search.h
+++ Search.h
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 // write a function to print the contents of your array
 //   bonus! try to implement your function so that it can accept
 //     many types of data (int, char, string, etc.)
-template <typename flexibleType> // ???
+template <typename flexibleType> // ??? 
 void printArray(flexibleType arr[], int size) 
 {
     for (int i = 0; i < size; i++)
@@ -60,21 +59,21 @@ bool binSearch(float target, float arr[], int n) {
 //   return false otherwise
 bool binSearchR(char target, char charray[], int n) {
     // base case
-if (n <= 0)
-{
-    return false;
-}
-    //general case
-int mid = n/2;
-if (charray [mid] == target)
-{
-    return true;
-}
-if (charray[mid] > target)
-{
-    return binSearchR(target,charray,mid);
-}
-    return binSearchR(target,&charray[mid + 1], n - mid - 1);
+    if (n <= 0)
+    {
+        return false;
+    }
+    // general case
+    int mid = n / 2;
+    if (charray[mid] == target)
+    {
+        return true;
+    }
+    if (charray[mid] > target)
+    {
+        return binSearchR(target, charray, mid);
+    }
+    return binSearchR(target, &charray[mid + 1], n - mid - 1);
 }
 
 // Implement a brand new sorting algorithm
@@ -82,22 +81,6 @@ if (charray[mid] > target)
 //    Be sure to do your best to implement the algorithm's logical steps
 // Step 1: implement a function swap() that will swap any two elements in an array,
 //          given their indices
-// Step 2: implement a function minFind() that will find and return 
-//          the **index** of the smallest element in an array
-/*
-Step 3: Finally, use your two functions above to complete the following in newSort():
-        1 - search through the array to find the location of the smallest value
-        2 - swap this value with the value at the start of the array
-        3 - the first element is now sorted! 
-            Redo your sort on the remaining elements (from index 1 to end)
-            On the next iteration, you should be swapping the smallest remaining value 
-                with the second index value in the array
-            ...then swapping the smallest remaining value with the third indexed value... etc.
-            
-        4 - continue iterating until you reach the end of the list
-        *** You can make this recursive, if you wish!
-*/
-
 void swap(double darray[], int i, int j) 
 {
     double temp = darray[i];
@@ -105,6 +88,8 @@ void swap(double darray[], int i, int j)
     darray[j] = temp;
 }
 
+// Step 2: implement a function minFind() that will find and return 
+//          the **index** of the smallest element in an array
 int minFind(double darray[], int n) 
 {
     int minIndex = 0;
@@ -116,16 +101,32 @@ int minFind(double darray[], int n)
     return minIndex;
 }
 
+// Step 3: Finally, use your two functions above to complete the following in newSort():
+//        1 - search through the array to find the location of the smallest value
+//        2 - swap this value with the value at the start of the array
+//        3 - the first element is now sorted! 
+//            Redo your sort on the remaining elements (from index 1 to end)
+//            On the next iteration, you should be swapping the smallest remaining value 
+//                with the second index value in the array
+//            ...then swapping the smallest remaining value with the third indexed value... etc.
+//            
+//        4 - continue iterating until you reach the end of the list
+//        *** You can make this recursive, if you wish!
 void SortHelper(double darray[], int n, int start) {
     if (start >= n - 1) {
         return;
     }
-    int minIndex = minFind(darray, start, n);
+    int minIndex = start;
+    for (int i = start + 1; i < n; ++i) {
+        if (darray[i] < darray[minIndex]) {
+            minIndex = i;
+        }
+    }
+    swap(darray, start, minIndex);
     SortHelper(darray, n, start + 1);
 }
 
 void newSort(double darray[], int n) 
 {
-     SortHelper(darray, n, 0);
+    SortHelper(darray, n, 0);
 }
-
